de8cc1202e9c3258dd747e683d255dfede2d7a81
Couth <craftcouth@gmail.com>
2023-12-05 11:44:19 -0300

    wip(10): refactor doing // fails on 22

--- doing.ts
+++ doing.ts
@@ -1,113 +1,124 @@
-const isValidChar = (currentChar: string, currentPatternChar: string) => {
-  return [currentChar, '.'].includes(currentPatternChar);
+const isStar = (pattern: string, index: number) => {
+  return pattern[index + 1] === '*';
 };
 
-const isStar = (currentPatternChar: string) => {
-  return currentPatternChar === '*';
+const isValidChar = (char: string, patternChar: string) => {
+  return [char, '.'].includes(patternChar);
 };
 
-const isValidStar = (
-  currentChar: string,
-  pattern: string,
-  patternIndex: number
-) => {
-  return isValidChar(currentChar, pattern[patternIndex - 1]);
+const isSameStar = (pattern: string, index: number) => {
+  return pattern[index] === pattern[index + 2] && isStar(pattern, index + 2);
 };
 
-const isPreviousStarStillValid = (
-  currentChar: string,
-  pattern: string,
-  patternIndex: number
-) => {
-  return isValidStar(currentChar, pattern, patternIndex - 1);
-};
-
-const isSkippableChar = (pattern: string, patternIndex: number) => {
-  return isStar(pattern[patternIndex + 1]);
-};
-
-const validate = (
-  currentChar: string,
-  pattern: string,
-  patternIndex: number
-): [boolean, number, boolean?] => {
-  const currentPattern = pattern[patternIndex];
-  if (isValidChar(currentChar, pattern[patternIndex])) {
-    return [true, 1];
-  }
-  if (isStar(pattern[patternIndex])) {
-    if (isValidStar(currentChar, pattern, patternIndex)) {
-      return [true, 1];
+const isRemainingOnlyStars = (pattern: string) => {
+  let i = 0;
+  while (i < pattern.length) {
+    if (!isStar(pattern, i)) {
+      return false;
     }
-    if (isPreviousStarStillValid(currentChar, pattern, patternIndex)) {
-      return [true, 0];
-    }
-    return [true, 1, true];
+    i += 2;
   }
-  if (isSkippableChar(pattern, patternIndex)) {
-    return [true, 2, true];
-  }
-  return [false, 0];
-};
-
-const validateRemainingPattern = (
-  currentChar: string,
-  pattern: string,
-  patternIndex: number
-): [boolean, number] => {
-  const currentPattern = pattern[patternIndex];
-  if (
-    isStar(pattern[patternIndex]) &&
-    isValidStar(currentChar, pattern, patternIndex)
-  ) {
-    return [true, 0];
-  }
-  if (
-    isValidChar(currentChar, pattern[patternIndex]) &&
-    isStar(pattern[patternIndex - 1]) &&
-    isValidStar(currentChar, pattern, patternIndex - 1)
-  ) {
-    return [true, 0];
-  }
-  if (isSkippableChar(pattern, patternIndex)) {
-    return [true, 1];
-  }
-  return [false, 0];
+  return true;
 };
 
 const isMatch = (s: string, pattern: string): boolean => {
   if (s === pattern) return true;
-  if (!pattern.includes('*') && s.length !== pattern.length) return false;
 
-  let matches = true;
+  let i = 0;
   let j = 0;
-  let isValid: boolean;
-  let increment: number;
-  let keepI: boolean | undefined;
-  for (let i = 0; i < s.length; i++) {
-    [isValid, increment, keepI] = validate(s[i], pattern, j);
-    if (!isValid) {
-      matches = false;
-      break;
-    } else {
-      j += increment;
-      if (keepI) {
-        i--; // to keep I
+  const skipped: string[] = [];
+  // while (j < pattern.length && i < s.length) {
+  while (j < pattern.length) {
+    if (isStar(pattern, j)) {
+      if (isSameStar(pattern, j)) {
+        pattern = pattern.substring(j + 2);
+        continue;
       }
+      if (s[i] && isValidChar(s[i], pattern[j])) {
+        skipped.push(pattern[j]);
+        do {
+          i++;
+        } while (s[i] && isValidChar(s[i], pattern[j]));
+      }
+      // if (!isValidChar(s[i], pattern[j]) && isValidChar(s[i - 1], pattern[j])) {
+      if (
+        pattern[j] === pattern[j + 2]
+        //|| skipped[0] === pattern[j]
+        //|| skipped[0] === pattern[j + 2]
+      ) {
+        i--;
+      }
+      pattern = pattern.substring(j + 2);
+      continue;
     }
+    if (s[i] && isValidChar(s[i], pattern[j])) {
+      // while (skipped.length > 0) {
+      //   if (skipped[0] === s[0]) {
+      //     do {
+      //       s = s.substring(1);
+      //     } while (skipped[0] === s[0]);
+      //     skipped.shift();
+      //   } else {
+      //     return false;
+      //   }
+      // }
+      // i = 0;
+      // pattern = pattern.substring(j + 1);
+      // continue;
+      s = s.substring(0, i) + s.substring(i + 1);
+      pattern = pattern.substring(j + 1);
+      while (skipped.length > 0 && s.length) {
+        if (s[0] && skipped[0] && isValidChar(s[0], skipped[0])) {
+          do {
+            s = s.substring(1);
+          } while (s[0] && skipped[0] && isValidChar(s[0], skipped[0]));
+        } else {
+          return false;
+        }
+        skipped.shift();
+      }
+      i = 0;
+      continue;
+    }
+    if (skipped.length > 0 && pattern.length > 0) {
+      do {
+        if (isValidChar(s[0], pattern[0])) {
+          s = s.substring(1);
+          pattern = pattern.substring(1);
+        } else {
+          if (s[0] && skipped[0] && isValidChar(s[0], skipped[0])) {
+            do {
+              s = s.substring(1);
+            } while (s[0] && skipped[0] && isValidChar(s[0], skipped[0]));
+          } else {
+            return false;
+          }
+          skipped.shift();
+        }
+      } while (skipped.length > 0);
+      continue;
+    }
+    // if (isStar(pattern, j)) {
+    //   pattern = pattern.substring(0, j) + pattern.substring(j + 2);
+    //   continue;
+    // }
+    return false;
+  }
+  i = 0;
+  while (skipped.length > 0 && s.length) {
+    // while (skipped.length > 0) {
+    if (s[i] && isValidChar(s[i], skipped[0])) {
+      do {
+        i++;
+      } while (s[i] && isValidChar(s[i], skipped[0]));
+      s = s.substring(i);
+    }
+    skipped.shift();
   }
 
-  const lastCharacter = s[s.length - 1];
-  for (j; j < pattern.length; j++) {
-    [isValid, increment] = validateRemainingPattern(lastCharacter, pattern, j);
-    if (!isValid) {
-      matches = false;
-      break;
-    }
-    j += increment;
-  }
-
-  return matches;
+  if (s.length > 0 || !isRemainingOnlyStars(pattern)) return false;
+  // if (s.length > 0) return false;
+  return true;
 };
 
 console.log(`1 - ${false === isMatch('aa', 'a') ? 'SUCESSO' : 'FAIL'}`);
@@ -133,3 +144,12 @@ console.log(
 console.log(`15 - ${false === isMatch('a', 'ab*a') ? 'SUCESSO' : 'FAIL'}`);
 console.log(`16 - ${true === isMatch('aaa', 'ab*a*c*a') ? 'SUCESSO' : 'FAIL'}`);
 console.log(`17 - ${true === isMatch('bbbba', '.*a*a') ? 'SUCESSO' : 'FAIL'}`);
+console.log(
+  `18 - ${false === isMatch('aaba', 'ab*a*aa*a*c*a') ? 'SUCESSO' : 'FAIL'}`
+);
+console.log(
+  `19 - ${false === isMatch('aaba', 'ab*a*c*a') ? 'SUCESSO' : 'FAIL'}`
+);
+console.log(`20 - ${true === isMatch('a', '.*.') ? 'SUCESSO' : 'FAIL'}`);
+console.log(`21 - ${false === isMatch('a', '.*..a*') ? 'SUCESSO' : 'FAIL'}`);
+console.log(`22 - ${true === isMatch('ab', '.*..') ? 'SUCESSO' : 'FAIL'}`);
